64558957cc78a61834d6a5e8febf63fad24f202b
Liset2005 <123668426+Liset2005@users.noreply.github.com>
2023-01-26 10:51:22 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Declare variables below to save the different sections (divs)                           story*/
+let StoryPart1=document.querySelector(".option-one-screen");
+let StoryPart2 =document.querySelector(".option-two-screen");
+let StoryPart1snowRace1=document.querySelector(".option-one-end");
+let StoryPartRacemountains2=document.querySelector(".option-two-end");
+let Snowrace1button=document.querySelector(".option-one");
+let Racemountains2=document.querySelector(".option-two");
+
+
+
+//when you're ready to make event handlers, uncoment the code below. Then                 fill in the blanks with the correct variables!
+
+
